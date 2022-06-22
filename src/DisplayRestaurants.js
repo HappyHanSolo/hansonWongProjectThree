225aece8abd860eab580d67f7757c919c046b0ed
Hanson Wong <hanson.wong01@gmail.com>
2022-06-22 17:03:48 -0400

    Fixed deployment files

--- src/DisplayRestaurants.js
+++ src/DisplayRestaurants.js
@@ -4,7 +4,7 @@ const DisplayRestaurants = (props) => {
             <section className="bobbaRestuarantResults">
                 <section className="gallery">
                     <div className="photoContainer">
-                        <img src={props.restuarantPic} alt={`Image of ${props.restuarantName}`}/>
+                        <img src={props.restuarantPic} alt={`Showcasing ${props.restuarantName} location and product`}/>
                         <h2>{props.restuarantName}</h2>
                         <h3>{props.restuarantLocation}</h3>
                         <p>{props.price}</p>
